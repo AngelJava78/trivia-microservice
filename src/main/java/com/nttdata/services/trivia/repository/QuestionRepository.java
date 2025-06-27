8b7d9571e591d6522eda80f04a45b157f5e237be
Angel Javier Valdez <angel_java@hotmail.com>
2025-06-26 19:38:18 -0600

    PMD issues were resolved

--- src/main/java/com/nttdata/services/trivia/repository/QuestionRepository.java
+++ src/main/java/com/nttdata/services/trivia/repository/QuestionRepository.java
@@ -1,8 +1,6 @@
 package com.nttdata.services.trivia.repository;
 
-import com.nttdata.services.trivia.dto.QuestionDto;
 import com.nttdata.services.trivia.model.Question;
-import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
